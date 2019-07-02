1f4d8b1e09f7a51dc19fffe74dd2f5c6dbf0e238
Darek Zbyrad <dariusz.zbyrad@gmail.com>
2019-07-02 14:52:41 +0200

    Added init project for the twitter API consumer project

---
+++ twitter-consumer/src/main/java/com/dariuszzbyrad/twitter/twitterconsumer/TwitterConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.dariuszzbyrad.twitter.twitterconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterConsumerApplication.class, args);
+    }
+
+}
---
+++ twitter-consumer/src/test/java/com/dariuszzbyrad/twitter/twitterconsumer/TwitterConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dariuszzbyrad.twitter.twitterconsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwitterConsumerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
