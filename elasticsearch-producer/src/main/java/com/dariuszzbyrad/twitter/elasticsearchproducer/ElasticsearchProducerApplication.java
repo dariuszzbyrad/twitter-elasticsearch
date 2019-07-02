88721cd769feaf5ea94b9e037dfb3f521f5333a7
Darek Zbyrad <dariusz.zbyrad@gmail.com>
2019-07-02 14:52:09 +0200

    Added init project for the elasticsearch producer project

---
+++ elasticsearch-producer/src/main/java/com/dariuszzbyrad/twitter/elasticsearchproducer/ElasticsearchProducerApplication.java
@@ -0,0 +1,13 @@
+package com.dariuszzbyrad.twitter.elasticsearchproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchProducerApplication.class, args);
+    }
+
+}
---
+++ elasticsearch-producer/src/test/java/com/dariuszzbyrad/twitter/elasticsearchproducer/ElasticsearchProducerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dariuszzbyrad.twitter.elasticsearchproducer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticsearchProducerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
